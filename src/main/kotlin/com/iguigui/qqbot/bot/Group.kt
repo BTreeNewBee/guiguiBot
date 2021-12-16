b89395fc244e241aeae1758624f98b58e6d4b8ee
guigui <atmzxl@163.com>
2021-12-16 22:52:42 +0800

    微信机器人支持

--- src/main/kotlin/com/iguigui/qqbot/bot/Group.kt
+++ src/main/kotlin/com/iguigui/qqbot/bot/Group.kt
@@ -2,4 +2,14 @@ package com.iguigui.qqbot.bot
 
 
 interface Group {
+
+    fun getMemberlist()
+
+    fun sendTextMessage(content: String)
+
+    fun getId() : String?
+
+    fun setId(id :String)
+
+
 }
