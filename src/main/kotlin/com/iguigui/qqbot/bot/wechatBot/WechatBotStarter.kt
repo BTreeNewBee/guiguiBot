851b55e454ab9a55596b22eea4142ffc813b3d36
guigui <atmzxl@163.com>
2021-12-16 22:55:45 +0800

    今天这个B班就上到这

--- src/main/kotlin/com/iguigui/qqbot/bot/wechatBot/WechatBotStarter.kt
+++ src/main/kotlin/com/iguigui/qqbot/bot/wechatBot/WechatBotStarter.kt
@@ -10,8 +10,6 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import javax.annotation.PostConstruct
 
-fun main() {
-}
 
 @Configuration
 open class WechatBotStarter {
