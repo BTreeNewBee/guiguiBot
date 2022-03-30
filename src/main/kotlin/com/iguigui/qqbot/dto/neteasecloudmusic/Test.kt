8a9739dc2adf01ba2809aebe9ca93343c8308e8d
guigui <atmzxl@163.com>
2022-03-30 23:58:12 +0800

    提醒名称变更

--- src/main/kotlin/com/iguigui/qqbot/dto/neteasecloudmusic/Test.kt
+++ src/main/kotlin/com/iguigui/qqbot/dto/neteasecloudmusic/Test.kt
@@ -4,19 +4,19 @@ import com.alibaba.fastjson.JSONObject
 import top.yumbo.util.music.MusicEnum
 import top.yumbo.util.music.musicImpl.netease.NeteaseCloudMusicInfo
 
-fun main() {
-    MusicEnum.setBASE_URL_163Music("http://192.168.50.185:3000")
-    val neteaseCloudMusicInfo = NeteaseCloudMusicInfo()
-
-    val json = JSONObject()
-    json["id"] = "7008518343"
-
-    val playlistDetail = neteaseCloudMusicInfo.playlistDetail(json).toJavaObject(PlaylistDetail::class.java)
-    playlistDetail.playlist?.trackIds?.forEach {
-
-    }
-
-}
+//fun main() {
+//    MusicEnum.setBASE_URL_163Music("http://192.168.50.185:3000")
+//    val neteaseCloudMusicInfo = NeteaseCloudMusicInfo()
+//
+//    val json = JSONObject()
+//    json["id"] = "7008518343"
+//
+//    val playlistDetail = neteaseCloudMusicInfo.playlistDetail(json).toJavaObject(PlaylistDetail::class.java)
+//    playlistDetail.playlist?.trackIds?.forEach {
+//
+//    }
+//
+//}
 
 
 suspend fun getTrackInfo() {
