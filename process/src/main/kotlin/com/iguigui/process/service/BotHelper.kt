c66fea11804ca0037f70a4d885f12e21a610a415
iguigui <atmzxl@163.com>
2022-12-09 15:57:53 +0800

    帮助菜单

--- process/src/main/kotlin/com/iguigui/process/service/BotHelper.kt
+++ process/src/main/kotlin/com/iguigui/process/service/BotHelper.kt
@@ -2,9 +2,14 @@ package com.iguigui.process.service
 
 import com.iguigui.common.annotations.SubscribeBotMessage
 import com.iguigui.process.handler.MessageDispatcher1
+import com.iguigui.process.qqbot.MessageAdapter
 import com.iguigui.process.qqbot.dto.GroupMessagePacketDTO
+import io.ktor.util.collections.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
+import java.util.LinkedHashMap
+import java.util.concurrent.ConcurrentHashMap
+import javax.annotation.PostConstruct
 
 
 @Component
@@ -13,13 +18,56 @@ class BotHelper {
     @Autowired
     lateinit var messageDispatcher: MessageDispatcher1
 
-    @SubscribeBotMessage("Bot帮助菜单", "帮助",false)
-    fun helper(dto: GroupMessagePacketDTO) {
-        messageDispatcher.handlerBeans.forEach { (method, _) ->
-            method.getAnnotation(SubscribeBotMessage::class.java).let {
-                //今天的特别编码行动就到此结束力
-            }
-        }
+    lateinit var methodList: List<SubscribeBotMessage>
+
+    @Autowired
+    lateinit var messageAdapter: MessageAdapter
+
+
+    val helpCache: MutableMap<Long, Any> = ConcurrentHashMap()
+
+    @PostConstruct
+    fun initMethodCache() {
+        methodList =
+            messageDispatcher.handlerBeans.map { it.key.getAnnotation(SubscribeBotMessage::class.java) }.toList()
     }
+
+    @SubscribeBotMessage("Bot帮助菜单", "帮助", false)
+    fun helper(dto: GroupMessagePacketDTO) {
+        val contentToString = dto.contentToString()
+        if (contentToString != "帮助"
+            && contentToString != "菜单"
+        ) {
+            return
+        }
+        helpCache[dto.sender.id] = Any()
+
+        var message = messageDispatcher.handlerBeans
+            .map { it.key.getAnnotation(SubscribeBotMessage::class.java) }
+            .filter { it.export }
+            .map { it.name }
+            .joinToString { "\n" }
+
+        message = "请输入序号选择功能详情：\n$message"
+
+        messageAdapter.sendGroupMessage(dto.sender.group.id, message)
+    }
+
+
+    @SubscribeBotMessage("Bot帮助菜单", "帮助", false)
+    fun help0(dto: GroupMessagePacketDTO) {
+        if (helpCache[dto.sender.id] == null) {
+            return
+        }
+        val id: Int?
+        try {
+            id = dto.contentToString().toInt()
+        } catch (e: NumberFormatException) {
+            return
+        }
+        helpCache.remove(dto.sender.id)
+        messageAdapter.sendGroupMessage(dto.sender.group.id, methodList[id].desc)
+    }
+
 
 }
